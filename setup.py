4d6c236624f2beeb8ed398ea62801159d9fe11ca
yu.zhang18 <yu.zhang18@nio.com>
2022-10-10 06:59:48 +0000

    version="0.1a16"

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ module_ext4 = Extension('droidtools.ext4fs',
 setup(
     name='python-ext4fs',
     packages= find_packages(),
-    version="0.1a15",
+    version="0.1a16",
     license='GPLv3 License',
     author='Tarek Galal',
     ext_modules = [module_ext4],
